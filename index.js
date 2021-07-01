1703f1127870e59c38042c485fe4508b0c4bca47
Franciscoandrade413 <francsicoandrade2001@gmail.com>
2021-07-01 10:23:29 -0700

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(nameArray, stringArg){
+    return nameArray.filter(function(person) {
+        return person.toUpperCase() === stringArg.toUpperCase();
+    })
+}
+
+function fuzzyMatch(nameArray, stringArg){
+    return nameArray.filter(function(person) {
+        return person.toLowerCase().indexOf(stringArg.toLowerCase()) == 0
+    })
+}
+
+function matchName(nameArray, stringArg){
+    return nameArray.filter(function(person){
+        return person.name === stringArg
+    })
+}
